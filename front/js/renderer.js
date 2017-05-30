4a8c0a03d7de4dbdadc3b1b1812d58b7e9bc8923
HeySora <contact@heysora.net>
2017-05-30 11:22:02 +0200

    Avancement de la page principale

--- front/js/renderer.js
+++ front/js/renderer.js
@@ -1,1 +1,8 @@
 $(document).foundation()
+
+function resize() {
+	$('#elements').height( $('#sidebar').innerHeight() - $('#sidebar-title').outerHeight() - 32 );
+}
+
+$(window).resize(resize);
+resize();
